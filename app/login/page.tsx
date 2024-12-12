10cfb471c64b327bed18a4b001ef696aa276eb4f
socrat <78218332+Socrat47@users.noreply.github.com>
2024-12-13 02:14:58 +0300

    Update

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,17 +1,17 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function Login() {
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="grid grid-cols-4 h-[700px] w-[1200px] rounded-xl overflow-hidden shadow-top-only">
-        <div className="bg-myprimary col-span-1">
+        <div className="bg-myprimary col-span-1 hidden lg:flex xl:flex md:flex">
           <div className="absolute mt-36 mr-12">
-            <Image src={"/ai.png"} alt="ai" width={400} height={400} />
+            <Image className="hidden lg:flex xl:flex" src={"/ai.png"} alt="ai" width={400} height={400} />
           </div>
         </div>
-        <div className="col-span-3">
+        <div className="col-span-3 ml-32 lg:ml-0 xl:ml-0 md:ml-0">
           <div className="flex flex-col items-center justify-center mt-16">
-            <div className="mr-18">
+            <div className="ml-22 md:mr-18 lg:mr-18 xl:mr-18">
               <div>
                 <h1 className="text-2xl font-semibold">Giriş</h1>
                 <label className="font-semibold text-xs opacity-80">
@@ -22,11 +22,11 @@ export default function Home() {
                 <form className="flex flex-col gap-5 mt-5">
                   <div className="flex flex-col gap-2">
                     <label className="text-sm font-semibold opacity-80">E-Posta</label>
-                    <input type="text" placeholder="E-Posta" className="w-[500px] h-10 rounded-md px-2 outline-none border border-gray-400 text-[16px] ml-1" />
+                    <input type="text" placeholder="E-Posta" className="w-[300px] md:w-[500px] lg:w-[500px] xl:w-[500px] h-10 rounded-md px-2 outline-none border border-gray-400 text-[16px] ml-1" />
                   </div>
                   <div className="flex flex-col gap-2">
                     <label className="text-sm font-semibold opacity-80">Şifre</label>
-                    <input type="text" placeholder="Şifre" className="w-[500px] h-10 rounded-md px-2 outline-none border border-gray-400 text-[16px] ml-1" />
+                    <input type="text" placeholder="Şifre" className="w-[300px] md:w-[500px] lg:w-[500px] xl:w-[500px] h-10 rounded-md px-2 outline-none border border-gray-400 text-[16px] ml-1" />
                   </div>
                 </form>
                 <div className="flex items-center justify-between mt-3">
